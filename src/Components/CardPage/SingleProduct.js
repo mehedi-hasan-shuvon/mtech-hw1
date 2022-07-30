d9b6e459d4dc2ea4b9c8af4898e189f14bb2b93b
mehedi hasan <mehedi.hasan.shuvon@gmail.com>
2022-07-31 04:43:48 +0600

    finished hw

--- src/Components/CardPage/SingleProduct.js
+++ src/Components/CardPage/SingleProduct.js
@@ -1,45 +1,126 @@
-import { Box, Stack, IconButton } from '@mui/material';
-import React from 'react';
-import EditIcon from '@mui/icons-material/Edit';
+import { Box, Stack, IconButton, Typography } from "@mui/material";
+import React from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
 
 const SingleProduct = ({ singleProduct }) => {
+  const RestproductName = singleProduct.name.slice(1);
 
-    return (
-        <Stack direction='column'
-            sx={{
-                height: 'auto',
-                minHeight: 250,
-                width: '100%',
-                maxWidth: { md: '20%', sm: '40%', xs: '100%' },
-                borderRadius: 3,
-                background: 'yellow',
-                boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
-            }}>
-            <Box component="img"
-                alt="This is a dog art"
-                flexGrow={1}
-                boxSizing="border-box"
-                flexWrap="wrap"
-                src={singleProduct.img}
-                sx={{
-                    height: 167,
-                    width: 250,
-                    // maxHeight: { xs: 233, md: 167 },
-                    // maxWidth: { xs: 350, md: 250 },
-                    margin: 'auto',
-                    borderRadius: 3,
-                    position: 'relative'
-                }} >
-            </Box>
+  function Abc() {
+    // switch (singleProduct.status) {
+    //   case "Free":
+    // <IconButton aria-label="download">
+    //   <FileDownloadIcon />
+    // </IconButton>;
+    //     break;
+    //   case "Premium":
+    //     <IconButton aria-label="download">
+    //       <FileDownloadIcon />
+    //     </IconButton>;
+    //     break;
+    // }
+    if (singleProduct.status == "Free") {
+      return (
+        <IconButton aria-label="download">
+          <LockOpenIcon />
+        </IconButton>
+      );
+    } else {
+      return (
+        <IconButton aria-label="download">
+          <LockIcon />
+        </IconButton>
+      );
+    }
+  }
 
-            <h4>{singleProduct.name}</h4>
-            <IconButton
-                aria-label="edit"
-                sx={{ bgcolor: '#fff', position: 'absolute', borderRadius: 1, ':hover': { bgcolor: '#f7f7f7' } }}>
-                <EditIcon />
-            </IconButton>
-        </Stack >
-    );
+  return (
+    <Stack
+      direction="column"
+      sx={{
+        height: "auto",
+        minHeight: 250,
+        width: "100%",
+        maxWidth: { md: "20%", sm: "40%", xs: "100%" },
+        borderRadius: 3,
+        position: "relative",
+        background: "white",
+        boxShadow:
+          "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+      }}
+    >
+      <Box
+        component="img"
+        alt="This is a dog art"
+        flexGrow={1}
+        boxSizing="border-box"
+        // flexWrap="wrap"
+        src={singleProduct.img}
+        sx={{
+          //   minHeight: { xs: 233, md: 167 },
+          //   minWidth: { xs: 233, md: 167 },
+          //   width: 300,
+          maxHeight: { xs: 233, sm: 250, md: 300 },
+          maxWidth: { xs: 550, sm: 600, md: 550 },
+          margin: "auto",
+          borderRadius: 3,
+          padding: 3,
+          borderRadius: 9,
+        }}
+      ></Box>
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{
+          position: "absolute",
+          borderRadius: 1,
+          right: 30,
+          top: 20,
+          ":hover": { bgcolor: "red" },
+        }}
+      >
+        {/* <IconButton aria-label="edit">
+          <EditIcon />
+        </IconButton> */}
+        {Abc()}
+        <Typography variant="p" color="gray">
+          {singleProduct.status}
+        </Typography>
+      </Stack>
+
+      <Stack
+        direction="row"
+        width="100%"
+        justifyContent="center"
+        alignItems="baseline"
+      >
+        <Typography variant="h3">{singleProduct.name.charAt(0)}</Typography>
+        <Typography variant="p" color="gray">
+          {RestproductName}
+        </Typography>
+      </Stack>
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ paddingLeft: 3, paddingBottom: 3 }}
+      >
+        <Typography
+          variant="h6"
+          sx={{ fontWeight: "bold", textDecoration: "underline" }}
+        >
+          Download
+        </Typography>
+        <IconButton aria-label="download">
+          <FileDownloadIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default SingleProduct;
--- src/fakeDb/Product.js
+++ src/fakeDb/Product.js
@@ -1,21 +1,20 @@
 export const Products = [
-    {
-        id: '0',
-        name: 'Dog illustration',
-        status: 'Premium',
-        img: 'https://i.ibb.co/xh7Ys2D/dog-art.jpg'
-    },
-    {
-        id: '1',
-        name: 'Astronaut riding',
-        status: 'Premium',
-        img: 'https://i.ibb.co/xh7Ys2D/dog-art.jpg'
-    },
-    {
-        id: '2',
-        name: 'Rockstar guitarist',
-        status: 'Free',
-        img: 'https://i.ibb.co/xh7Ys2D/dog-art.jpg'
-
-    },
-]
+  {
+    id: "0",
+    name: "Dog illustration",
+    status: "Premium",
+    img: "https://i.ibb.co/xh7Ys2D/dog-art.jpg",
+  },
+  {
+    id: "1",
+    name: "Astronaut riding",
+    status: "Premium",
+    img: "https://i.ibb.co/xh7Ys2D/dog-art.jpg",
+  },
+  {
+    id: "2",
+    name: "Rockstar guitarist",
+    status: "Free",
+    img: "https://i.ibb.co/xh7Ys2D/dog-art.jpg",
+  },
+];
